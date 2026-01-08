25de0e7c29fcd49c6fc8b7c9bc1dc2cab0fa9bc5
D. Gabriel <mendesdouglasgabriel30@gmail.com>
2026-01-08 16:42:32 -0300

    chore: initial Spring Boot project with PostgreSQL

---
+++ src/main/java/com/gabriel/api_habitos/ApiHabitosApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.api_habitos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiHabitosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiHabitosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/api_habitos/ApiHabitosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.api_habitos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiHabitosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
